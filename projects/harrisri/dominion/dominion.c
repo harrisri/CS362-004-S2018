3905e697767fb80d569434549276168d81cda6cb
harrisri <harrisri@oregonstate.edu>
2018-06-04 20:08:55 -0700

    Update dominion.c

--- projects/harrisri/dominion/dominion.c
+++ projects/harrisri/dominion/dominion.c
@@ -1251,7 +1251,7 @@ int discardCard(int handPos, int currentPlayer, struct gameState *state, int tra
   if (trashFlag < 1)
     {
       //add card to played pile
-      state->playedCards[state->playedCardCount] = state->hand[currentPlayer][handPos]; 
+      //state->playedCards[state->playedCardCount] = state->hand[currentPlayer][handPos]; 
       state->playedCardCount++;
     }
 	
